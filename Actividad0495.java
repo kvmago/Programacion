dae0be25f9aeaf7270baeff20af797bf0668eeeb
Kevin Martinez <kevin.martinez.dam1@iessanandres.com>
2016-10-06 10:46:59 +0200

    Actividad 0607

--- Actividad0495.java
+++ Actividad0495.java
@@ -2,5 +2,6 @@ public class Actividad0495{
 	public static void main (String args[]){
 	System.out.println("Kevin Martínez González");
 	System.out.println("20");
+	System.out.println("León");
 	}
 }
